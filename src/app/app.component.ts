0e93f791f208688e6ab1258274e6ee353abe792a
uriel twito <uriel.t@sapiens.com>
2017-02-10 21:38:52 +0200

    work with actions supplied outside the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ApplicationAction} from "./components/round-menu/round-menu.component";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  actions: ApplicationAction<any>[] = [
+    new ApplicationAction<any>("aa", "aa", "aa", this.action.bind(this), "icon-home"),
+    new ApplicationAction<any>("bb", "bb", "bb", this.action.bind(this), "icon-headphones"),
+    new ApplicationAction<any>("cc", "cc", "cc", this.action.bind(this), "icon-star"),
+    new ApplicationAction<any>("dd", "dd", "dd", this.action.bind(this), "icon-envelope-alt"),
+    new ApplicationAction<any>("ee", "ee", "ee", this.action.bind(this), "icon-cloud"),
+    new ApplicationAction<any>("ff", "ff", "ff", this.action.bind(this), "icon-heart"),
+    new ApplicationAction<any>("gg", "gg", "gg", this.action.bind(this), "icon-user")
+
+  ];
+
+  action(name: string) {
+    console.log("execute", name);
+  }
+
+  click() {
+    console.log("timeout");
+    setTimeout(() => {
+
+    });
+  }
 }
--- src/app/components/round-menu/round-menu.component.ts
+++ src/app/components/round-menu/round-menu.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, Renderer, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'cool-round-menu',
   templateUrl: './round-menu.component.html',
   styleUrls: ['./round-menu.component.css']
 })
-export class RoundMenuComponent implements OnInit {
+export class RoundMenuComponent {
 
-  constructor() { }
+  MAX_ITEMS: number = 5;
+  _actions: ApplicationAction<any>[];
+  currentActionPos: number = 0;
 
-  ngOnInit() {
+
+  @Input()
+  set actions(actions: ApplicationAction<any>[]) {
+    this._actions = actions;
+  }
+
+  constructor(private elementref: ElementRef, private renderer: Renderer) {
+    this.currentActionPos = 0;
+  }
+
+  public nextItems() {
+    if (this.currentActionPos + this.MAX_ITEMS >= this.actions.length) {
+      this.currentActionPos = 0;
+    }
+    else {
+      this.currentActionPos = (this.currentActionPos + this.MAX_ITEMS);
+    }
+  }
+
+  get partialActions(): ApplicationAction<any>[] {
+    return this.actions.slice(this.currentActionPos, this.currentActionPos + this.MAX_ITEMS);
+  }
+
+  get actions(): ApplicationAction<any>[] {
+    return this._actions;
+  }
+
+  private execute(action) {
+    action.execute();
+  }
+
+}
+
+
+export class ApplicationAction<T> {
+  public subActions: Array<ApplicationAction<T>>;
+  public canExecute: (T) => boolean;
+
+  constructor(public actionParameter: T,
+              public title: string,
+              public tooltip: string,
+              public action: (T) => void,
+              public icon?: string) {
+    this.subActions = new Array<ApplicationAction<T>>();
+    this.canExecute = (T) => {
+      return true;
+    }
+  }
+
+  execute() {
+    this.action(this.actionParameter);
+  }
+
+  executeWitParameter(parameter: any) {
+    this.action(parameter);
   }
 
 }
